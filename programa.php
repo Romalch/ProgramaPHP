218847227a92dd1cfd1533093eecfbdcbb905d3c
Mauricio Aldana <romalch@gmail.com>
2024-03-12 02:15:44 +0000

    Primer carga del codigo del programa en PHP

---
+++ programa.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Hola Mundo !!!</h1>";
+echo "<h2>9615670 - Ronald Mauricio Aldana Chacón</h2>";
+echo "<img src=\"casaantigua.jpg\" alt=\"Casa antigua, como la de mis abuelos, generada con Leonardo IA\" width=\"400\" height=\"341\" />"
+?>
